2c95206311fd3f35f0cb55c177fb06c299195a5c
jeff-hf <71419007+jeff-hf@users.noreply.github.com>
2021-07-19 12:27:16 -0600

    r

--- js/index.js
+++ js/index.js
@@ -2,3 +2,21 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+$(document).ready(function() {
+
+    $('.ir-arriba').click(function() {
+        $('body, html').animate({
+            scrollTop: '0px'
+        }, 300);
+    });
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 0) {
+            $('.ir-arriba').slideDown(300);
+        } else {
+            $('.ir-arriba').slideUp(300);
+        }
+    });
+
+});
